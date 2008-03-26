01de681cf950b75f971d401d346cb25b1b7aa1ff
Robert Lemke <robert@typo3.org>
2008-03-26 18:07:32 +0000

    Global change: Component configurations file format is now .php and not .ini anymore!
    * FLOW3: Refactored the component configuration parsing and moved it to a dedicated configuration builder.
    * Adapted the Components.ini of all v5 dist packages.

---
+++ Configuration/Components.php
@@ -0,0 +1,19 @@
+<?php
+declare(ENCODING="utf-8");
+
+/*                                                                        *
+ * Components configuration for the phpCR package                         *
+ *                                                                        */
+
+/**
+ * @package phpCR
+ * @version $Id:$
+ */
+
+$c->F3_phpCR_ItemInterface->scope = 'prototype';
+$c->F3_phpCR_NodeInterface->scope = 'prototype';
+$c->F3_phpCR_NodeIteratorInterface->scope = 'prototype';
+$c->F3_phpCR_PropertyInterface->scope = 'prototype';
+$c->F3_phpCR_PropertyIteratorInterface->scope = 'prototype';
+$c->F3_phpCR_ValueInterface->scope = 'prototype';
+$c->F3_phpCR_NodeTypeInterface->scope = 'prototype';
