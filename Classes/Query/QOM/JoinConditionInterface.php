18a7b74950b0ae9c9e3f6cf7c1884a1b8c182442
Karsten Dambekalns <karsten@typo3.org>
2009-11-02 15:13:10 +0000

    [~TASK] PHPCR: Added some more @api annotations at class level.

--- Classes/Query/QOM/JoinConditionInterface.php
+++ Classes/Query/QOM/JoinConditionInterface.php
@@ -28,6 +28,7 @@ namespace F3\PHPCR\Query\QOM;
  * @version $Id$
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
  * @license http://opensource.org/licenses/bsd-license.php Simplified BSD License
+ * @api
  */
 interface JoinConditionInterface {
 }
--- Classes/Version/VersionException.php
+++ Classes/Version/VersionException.php
@@ -12,7 +12,7 @@ namespace F3\PHPCR\Version;
  *                                                                        *
  * This script is distributed in the hope that it will be useful, but     *
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
-* TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
  * General Public License for more details.                               *
  *                                                                        *
  * You should have received a copy of the GNU Lesser General Public       *
