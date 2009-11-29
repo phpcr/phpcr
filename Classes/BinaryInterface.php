af1d7561c139ccd721904849da0603affd523c6f
Karsten Dambekalns <karsten@typo3.org>
2009-11-29 16:46:05 +0000

    [~API] PHPCR: BinaryInterface::read() now takes an optional third parameter to limit the number of bytes returned.

--- Classes/BinaryInterface.php
+++ Classes/BinaryInterface.php
@@ -47,10 +47,12 @@ interface BinaryInterface {
 
 	/**
 	 * Reads successive bytes from the specified position in this Binary into
-	 * the passed string until the end of the Binary is encountered.
+	 * the passed string until $limit or the end of the Binary is encountered
+	 * (whichever comes first).
 	 *
 	 * @param string $bytes the buffer into which the data is read.
 	 * @param integer $position the position in this Binary from which to start reading bytes.
+	 * @param integer $limit how many bytes to read, unlimited by default
 	 * @return integer the number of bytes read into the buffer
 	 * @throws \RuntimeException if an I/O error occurs.
 	 * @throws \InvalidArgumentException if offset is negative.
@@ -58,7 +60,7 @@ interface BinaryInterface {
 	 * @throws \F3\PHPCR\RepositoryException if another error occurs.
 	 * @api
 	 */
-	public function read(&$bytes, $position);
+	public function read(&$bytes, $position, $limit = 0);
 
 	/**
 	 * Returns the size of this Binary value in bytes.
