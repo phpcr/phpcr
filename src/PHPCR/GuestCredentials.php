eab49b053e98d70bd3e4a0370a2cea3f89f2efdf
Bastian Feder <lapis@bastian-feder.de>
2010-11-17 19:44:45 +0100

    Added page level docblocks, tidied the docu and corrected several annotations.

--- src/PHPCR/GuestCredentials.php
+++ src/PHPCR/GuestCredentials.php
@@ -1,37 +1,45 @@
 <?php
-declare(ENCODING = 'utf-8');
-namespace PHPCR;
-
-/*                                                                        *
- * This file was ported from the Java JCR API to PHP by                   *
- * Karsten Dambekalns <karsten@typo3.org> for the FLOW3 project.          *
- *                                                                        *
- * It is free software; you can redistribute it and/or modify it under    *
- * the terms of the GNU Lesser General Public License as published by the *
- * Free Software Foundation, either version 3 of the License, or (at your *
- * option) any later version. Alternatively, you may use the Simplified   *
- * BSD License.                                                           *
- *                                                                        *
- * This script is distributed in the hope that it will be useful, but     *
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
- * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
- * General Public License for more details.                               *
- *                                                                        *
- * You should have received a copy of the GNU Lesser General Public       *
- * License along with the script.                                         *
- * If not, see http://www.gnu.org/licenses/lgpl.html                      *
- *                                                                        *
- * The TYPO3 project - inspiring people to share!                         *
- *                                                                        */
-
 /**
- * GuestCredentials implements the Credentials interface and is used to obtain
- * a "guest", "public" or "anonymous" session.
- * Note that the characteristics of the session created from the
- * GuestCredentials remain implementation specific.
+ * Implementation of the CredentialsInterface to obtain a special type of session.
+ *
+ * This file was ported from the Java JCR API to PHP by
+ * Karsten Dambekalns <karsten@typo3.org> for the FLOW3 project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version. Alternatively, you may use the Simplified
+ * BSD License.
+ *
+ * This script is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with the script.
+ * If not, see {@link http://www.gnu.org/licenses/lgpl.html}.
+ *
+ * The TYPO3 project - inspiring people to share!
  *
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
  * @license http://opensource.org/licenses/bsd-license.php Simplified BSD License
+ *
+ * @package phpcr
+ * @subpackage prototypes
+ */
+
+declare(ENCODING = 'utf-8');
+namespace PHPCR;
+
+/**
+ * GuestCredentials is used to obtain a "guest", "public" or "anonymous" session.
+ *
+ * Note that the characteristics of the session created from the
+ * GuestCredentials remain implementation specific.
+ *
+ * @package phpcr
+ * @subpackage prototypes
  * @scope prototype
  */
 final class GuestCredentials implements \PHPCR\CredentialsInterface {}
