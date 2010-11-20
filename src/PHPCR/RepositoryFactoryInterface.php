d7d7b1b9cccfd41bff71a542d7a685265a8404ee
Bastian Feder <lapis@bastian-feder.de>
2010-11-20 21:40:46 +0100

    documentation cleanup

--- src/PHPCR/RepositoryFactoryInterface.php
+++ src/PHPCR/RepositoryFactoryInterface.php
@@ -1,28 +1,36 @@
 <?php
+/**
+ * Interface to describe the contract to implement a repository factory.
+ *
+ * This file was ported from the Java JCR API to PHP by
+ * Karsten Dambekalns <karsten@typo3.org> for the FLOW3 project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version. Alternatively, you may use the Simplified
+ * BSD License.
+ *
+ * This script is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with the script.
+ * If not, see {@link http://www.gnu.org/licenses/lgpl.html}.
+ *
+ * The TYPO3 project - inspiring people to share!
+ *
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ * @license http://opensource.org/licenses/bsd-license.php Simplified BSD License
+ *
+ * @package phpcr
+ * @subpackage interfaces
+ */
+
 declare(ENCODING = 'utf-8');
 namespace PHPCR;
-
-/*                                                                        *
- * This file was ported from the Java JCR API to PHP by                   *
- * Karsten Dambekalns <karsten@typo3.org> for the FLOW3 project.          *
- *                                                                        *
- * It is free software; you can redistribute it and/or modify it under    *
- * the terms of the GNU Lesser General Public License as published by the *
- * Free Software Foundation, either version 3 of the License, or (at your *
- * option) any later version. Alternatively, you may use the Simplified   *
- * BSD License.                                                           *
- *                                                                        *
- * This script is distributed in the hope that it will be useful, but     *
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
- * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
- * General Public License for more details.                               *
- *                                                                        *
- * You should have received a copy of the GNU Lesser General Public       *
- * License along with the script.                                         *
- * If not, see http://www.gnu.org/licenses/lgpl.html                      *
- *                                                                        *
- * The TYPO3 project - inspiring people to share!                         *
- *                                                                        */
 
 /**
  * RepositoryFactory is an interface for factory class implementations for
@@ -37,8 +45,8 @@ namespace PHPCR;
  * Get a default repository available in this environment:
  *    $repo = \PHPCR\RepositoryFactory::getRepository();
  *
- * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
- * @license http://opensource.org/licenses/bsd-license.php Simplified BSD License
+ * @package phpcr
+ * @subpackage interfaces
  * @api
  */
 interface RepositoryFactoryInterface {
@@ -60,8 +68,10 @@ interface RepositoryFactoryInterface {
      * should throw an RepositoryException if it is the right factory but has
      * trouble connecting to the repository.
      *
-     * @param array|NULL $parameters string key/value pairs as repository arguments or NULL if a client wishes to connect to a default repository.
-     * @return \PHPCR\RepositoryInterface a repository instance or NULL if this implementation does not understand the passed parameters
+     * @param array|NULL $parameters string key/value pairs as repository arguments or NULL if a client wishes
+     *                               to connect to a default repository.
+     * @return \PHPCR\RepositoryInterface a repository instance or NULL if this implementation does
+     *                                    not understand the passed parameters
      * @throws \PHPCR\RepositoryException if no suitable repository is found or another error occurs.
      * @api
      */
