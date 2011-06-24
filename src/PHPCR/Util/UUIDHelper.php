b661a5fda2eed93bd0cf200eb7bcf3024479ef8a
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-06-24 13:32:56 +0200

    typo fix

--- src/PHPCR/Util/UUIDHelper.php
+++ src/PHPCR/Util/UUIDHelper.php
@@ -37,7 +37,7 @@ class UUIDHelper
      */
     static public function isUUID($id)
     {
-        // UUDID is HEX_CHAR{8}-HEX_CHAR{4}-HEX_CHAR{4}-HEX_CHAR{4}-HEX_CHAR{12}
+        // UUID is HEX_CHAR{8}-HEX_CHAR{4}-HEX_CHAR{4}-HEX_CHAR{4}-HEX_CHAR{12}
         if (1 === preg_match('/^[[:xdigit:]]{8}-[[:xdigit:]]{4}-[[:xdigit:]]{4}-[[:xdigit:]]{4}-[[:xdigit:]]{12}$/', $id)) {
             return true;
         }
