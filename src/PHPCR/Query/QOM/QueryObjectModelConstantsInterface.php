882056aea2f8e0302d43842156c481b4bbdc9d89
dbu <david.buchmann@liip.ch>
2011-10-13 20:26:01 +0200

    aligning QOM constants with jcr 2.0 for compatibility

--- src/PHPCR/Query/QOM/QueryObjectModelConstantsInterface.php
+++ src/PHPCR/Query/QOM/QueryObjectModelConstantsInterface.php
@@ -41,73 +41,73 @@ interface QueryObjectModelConstantsInterface
      * An inner join.
      * @api
     */
-    const JCR_JOIN_TYPE_INNER = '{http://www.jcp.org/jcr/1.0}joinTypeInner';
+    const JCR_JOIN_TYPE_INNER = 'jcr.join.type.inner';
 
     /**
      * A left-outer join.
      * @api
     */
-    const JCR_JOIN_TYPE_LEFT_OUTER = '{http://www.jcp.org/jcr/1.0}joinTypeLeftOuter';
+    const JCR_JOIN_TYPE_LEFT_OUTER = 'jcr.join.type.left.outer';
 
     /**
      * A right-outer join.
      * @api
     */
-    const JCR_JOIN_TYPE_RIGHT_OUTER = '{http://www.jcp.org/jcr/1.0}joinTypeRightOuter';
+    const JCR_JOIN_TYPE_RIGHT_OUTER = 'jcr.join.type.right.outer';
 
     /**
      * The '=' comparison operator.
      * @api
     */
-    const JCR_OPERATOR_EQUAL_TO = '{http://www.jcp.org/jcr/1.0}operatorEqualTo';
+    const JCR_OPERATOR_EQUAL_TO = 'jcr.operator.equal.to';
 
     /**
      * The '!=' comparison operator.
      * @api
     */
-    const JCR_OPERATOR_NOT_EQUAL_TO = '{http://www.jcp.org/jcr/1.0}operatorNotEqualTo';
+    const JCR_OPERATOR_NOT_EQUAL_TO = 'jcr.operator.not.equal.to';
 
     /**
      * The '<' comparison operator.
      * @api
     */
-    const JCR_OPERATOR_LESS_THAN = '{http://www.jcp.org/jcr/1.0}operatorLessThan';
+    const JCR_OPERATOR_LESS_THAN = 'jcr.operator.less.than';
 
     /**
      * The '<=' comparison operator.
      * @api
     */
-    const JCR_OPERATOR_LESS_THAN_OR_EQUAL_TO = '{http://www.jcp.org/jcr/1.0}operatorLessThanOrEqualTo';
+    const JCR_OPERATOR_LESS_THAN_OR_EQUAL_TO = 'jcr.operator.less.than.or.equal.to';
 
     /**
      * The '>' comparison operator.
      * @api
     */
-    const JCR_OPERATOR_GREATER_THAN = '{http://www.jcp.org/jcr/1.0}operatorGreaterThan';
+    const JCR_OPERATOR_GREATER_THAN = 'jcr.operator.greater.than';
 
     /**
      * The '>=' comparison operator.
      * @api
     */
-    const JCR_OPERATOR_GREATER_THAN_OR_EQUAL_TO = '{http://www.jcp.org/jcr/1.0}operatorGreaterThanOrEqualTo';
+    const JCR_OPERATOR_GREATER_THAN_OR_EQUAL_TO = 'jcr.operator.greater.than.or.equal.to';
 
     /**
      * The 'like' comparison operator.
      * @api
     */
-    const JCR_OPERATOR_LIKE = '{http://www.jcp.org/jcr/1.0}operatorLike';
+    const JCR_OPERATOR_LIKE = 'jcr.operator.like';
 
     /**
      * Ascending order.
      * @api
     */
-    const JCR_ORDER_ASCENDING = '{http://www.jcp.org/jcr/1.0}orderAscending';
+    const JCR_ORDER_ASCENDING = 'jcr.order.ascending';
 
     /**
      * Descending order.
      * @api
     */
-    const JCR_ORDER_DESCENDING = '{http://www.jcp.org/jcr/1.0}orderDescending';
+    const JCR_ORDER_DESCENDING = 'jcr.order.descending';
 
     /**#@-*/
 }
