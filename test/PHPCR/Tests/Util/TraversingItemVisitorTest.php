8bf0475638b48855023e1ca07dbb245ee6e0b141
dbu <david.buchmann@liip.ch>
2011-11-17 15:21:53 +0100

    add test for uuid helper and empty dummy test for the traversing item visitor

---
+++ test/PHPCR/Tests/Util/TraversingItemVisitorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PHPCR\Tests\Util;
+
+use PHPCR\Util\TraversingItemVisitor;
+
+class TraversingItemVisitorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testVisitor()
+    {
+        $this->markTestSkipped('TODO: implement tests for breath and depth first and with and without limit');
+    }
+}
