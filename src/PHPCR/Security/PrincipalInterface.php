8e92221e32a065adf59609904937a7d17ca92f82
David Buchmann <david.buchmann@liip.ch>
2012-07-25 11:13:17 +0200

    changing hashCode method of principal to int to stay compatible with java, add methods to principal and document a bit

--- src/PHPCR/Security/PrincipalInterface.php
+++ src/PHPCR/Security/PrincipalInterface.php
@@ -27,24 +27,46 @@
 namespace PHPCR\Security;
 
 /**
- * As there are no acl standard interfaces in PHP this interface provides the
+ * As there are no ACL standard interfaces in PHP this interface provides the
  * Principal interface similar to the java.security.Principal
  *
  * The Principal is any entity that can be assigned privileges. E.g. a person,
  * a role, a computer.
  *
- * The reason to have this interface is that the phpcr implementation needs to
+ * The reason to have this interface is that the PHPCR implementation needs to
  * store the principals and use them on later requests.
  */
 interface PrincipalInterface
 {
+    /**
+     * Compares this principal to the passed object. Returns true if both this
+     * principal and the passed object match the same thing.
+     *
+     * This is necessary, as the same hashCode does not guarantee equality, and
+     * the === operator is too strict, as there could be two instances of the
+     * same principal.
+     *
+     * @param mixed $object
+     *
+     * @return boolean true if the principal passed to the method is the same
+     *      as this object
+     */
+    function equals($object);
+
     /**
      * The hash code must be the same for the same principal.
      *
      * However it should be unique inside your application for different
      * principals.
      *
-     * @return string a hashcode for this principal.
+     * @return int a hashcode for this principal.
      */
     function hashCode();
+
+    /**
+     * Returns the name of this principal.
+     *
+     * @return string name of this principal
+     */
+    function getName();
 }
