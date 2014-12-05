82abeacbd0d050e48ee44e2298346a750146f45c
David Buchmann <david.buchmann@liip.ch>
2014-12-05 18:21:10 +0100

    adjust repository factory to throw exception instead of returning null

---
+++ src/PHPCR/ConfigurationException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PHPCR;
+
+use InvalidArgumentException;
+
+/**
+ * Exception to throw when the options passed to the factory are invalid.
+ *
+ * @license http://www.apache.org/licenses Apache License Version 2.0, January 2004
+ * @license http://opensource.org/licenses/MIT MIT License
+ */
+class ConfigurationException extends InvalidArgumentException
+{
+}
